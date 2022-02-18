075ea2e05e5cff6d1ce65f393ecddccf042d8592
Mohamed Elbadawii <badawymohamed131@gmail.com>
2022-02-18 10:46:44 +0200

    Display api books

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,14 @@
-const Book = () => {
+const Book = ( props ) => {
+
+    const { book } = props;
 
     return (
         <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select value={ book.shelf }>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -15,8 +17,8 @@ const Book = () => {
               </select>
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{ book.title }</div>
+          <div className="book-authors">{ book.authors && book.authors.join(', ') }</div>
         </div>
       </li>
     )
